7525d07f5eb5ccb3fca163763c58aaea217097a8
Yuta iwasaki <yanyong0127@gmail.com>
2021-07-04 17:17:21 +0900

    失敗

---
+++ passingValueTableViewAndAlert/PassingValueViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PassingValueViewController.swift
+//  passingValueTableViewAndAlert
+//
+//  Created by IwasakIYuta on 2021/07/04.
+//
+
+import UIKit
+
+class PassingValueViewController: UIViewController {
+private var passingValue = ""
+    
+    @IBOutlet weak var passingValueLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        passingValueLabel.text = passingValue
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- passingValueTableViewAndAlert/ViewController.swift
+++ passingValueTableViewAndAlert/ViewController.swift
@@ -3,17 +3,43 @@
 //  passingValueTableViewAndAlert
 //
 //  Created by IwasakIYuta on 2021/07/04.
-//
-
+//ライブラリ「Eurekaを使って今度やってみるやりたいことできないおおお
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
-
+    @IBAction func passingValueButton(_ sender: UIButton) {
+        let passingValueTextField = self.view.viewWithTag(1) as! UITextField
+        let vc = self.storyboard?.instantiateViewController(identifier: "PassingValueView") as! PassingValueViewController
+        vc.passingValueLabel.text = passingValueTextField.text ?? ""
+    present(vc, animated: true, completion: nil)
+    }
+    
 }
-
+extension ViewController : UITableViewDataSource , UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell()
+//        let textField = cell.viewWithTag(1) as! UITextField
+        let button = cell.viewWithTag(2) as! UIButton
+        //textField.text = ""
+        passingValueButton(button)
+        
+        return cell
+    }
+    
+    
+}
+ 
